2b1f7bfdaf572af333a42328752f13e3a86cd87c
Mohammad Amil Bhat <61305746+amilbhat@users.noreply.github.com>
2023-10-26 22:58:49 +0530

    Updated Design and render details for each show

--- TV Show Search/script.js
+++ TV Show Search/script.js
@@ -1,20 +1,34 @@
 const form = document.querySelector("#searchForm")
+const shows = document.getElementById("shows")
 
 form.addEventListener("submit", async function(e) {
     e.preventDefault()
     const searchTerm = form.elements.query.value
     const response = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
-    makeImages(response.data)
+    // const response = await axios.get("./sample.json")
+    let showsHtml = makeImages(response.data)
+    shows.innerHTML = showsHtml
+    console.log(response.data[0])
     form.elements.query.value = ""
     
 })
 
 const makeImages = (shows) => {
+    showsHtml = ""
     for (let result of shows) {
         if (result.show.image) {
-            const img = document.createElement("IMG")
-            img.src = result.show.image.medium
-            document.getElementById("shows").append(img)
+            showsHtml += `
+                <div class="show">
+                    <img class="show-img" src=${result.show.image.medium}>
+                    <div class="show-details">
+                        <h2>${result.show.name}</h2>
+                        <h3>Genres: <span class="show-genres">${result.show.genres.join(", ")}</span></h3>
+                        <h3>Rating: <span class="show-rating">${result.show.rating.average !== null ? result.show.rating.average: "-"}</span></h3>
+                        <button><a href=${result.show.url}>Know More</a></button>
+                    </div>
+                </div>
+            `
         }
     }
+    return showsHtml
 }
