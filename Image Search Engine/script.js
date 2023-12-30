656360420e4ae4f99177f21d425fb53087b73dfb
Mohammad Amil Bhat <bhatamil14@gmail.com>
2023-12-30 09:37:15 +0530

    Connected Unsplash API to fetch and show images

--- Image Search Engine/script.js
+++ Image Search Engine/script.js
@@ -1,5 +1,47 @@
-const searchForm = document.getElementById("search-form")
-const searchBox = document.getElementById("search-box")
-const searchResult = document.getElementById("search-result")
-const showMoreBtn = document.getElementById("show-more-btn")
+const accessKey = "WIjg896OvcwXvTON0pqdPnbgl92JXvHm-qhOclzvc-8";
 
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+
+async function searchImages() {
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (page === 1) {
+        searchResult.innerHTML = "";
+    }
+
+    const results = data.results;
+    results.map((result) => {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+}
+
+searchForm.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    page = 1;
+    searchImages();
+    showMoreBtn.style.display = "block";   
+
+})
+
+showMoreBtn.addEventListener("click",  () => {
+    page++;
+    searchImages();
+})
